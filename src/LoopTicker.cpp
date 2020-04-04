bf599291e23ecc51853ddca3dd8d50ffa6acbef3
Rafa Couto <caligari@treboada.net>
2020-04-04 15:18:08 +0200

    Importing library.

---
+++ src/LoopTicker.cpp
@@ -0,0 +1,49 @@
+
+#include "LoopTicker.hpp"
+
+
+#ifdef ARDUINO
+#   include <Arduino.h>
+#endif
+
+
+void LoopTicker::_updateLoopMillis()
+{
+#   ifdef ARDUINO
+
+        uint32_t millis32 = millis();
+        if (millis32 < _loopMsLow) _loopMsHigh++;
+        _loopMsLow = millis32;
+
+#   else
+
+        // implement here: non-arduino time milliseconds adquisition
+
+#       error "LoopTicker::_updateLoopMillis() not implemented"
+
+#   endif
+}
+
+
+LoopTicker::LoopTicker(TaskEntryPoint tasks[], uint8_t size)
+{
+    _tasks = tasks;
+    _tasksCount = size;
+
+    _updateLoopMillis();
+}
+
+
+void LoopTicker::doLoop()
+{
+    // time at loop beginning
+    _updateLoopMillis();
+
+    // execute each task entrypoint
+    for (uint8_t t = 0; t < _tasksCount; t++)
+    {
+        void* data = _tasks[t].data;
+        (_tasks[t].function)(data, this);
+    }
+}
+
---
+++ src/LoopTicker.hpp
@@ -0,0 +1,35 @@
+
+#ifndef _LOOP_TICKER_HPP
+#define _LOOP_TICKER_HPP
+
+#include <stdint.h>
+
+class LoopTicker
+{
+    public:
+
+        struct TaskEntryPoint
+        {
+            void (*function)(void* data, LoopTicker* loop_ticker);
+            void* data;
+        };
+
+        LoopTicker(TaskEntryPoint task_entry_points[], uint8_t size);
+
+        void doLoop();
+
+        uint32_t getLoopMs32() { return _loopMsLow; }
+        uint64_t getLoopMs64() { return ((uint64_t)_loopMsHigh) << 32 | _loopMsLow; }
+
+    private:
+
+        uint32_t _loopMsHigh;
+        uint32_t _loopMsLow;
+
+        TaskEntryPoint* _tasks;
+        uint8_t _tasksCount;
+
+        void _updateLoopMillis();
+};
+
+#endif // _LOOP_TICKER_HPP
